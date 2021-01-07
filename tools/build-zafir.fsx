a3f27da45675a1dcc00f73322635e7b923c55f24
András Jankó <andras.janko@gmail.com>
2021-01-07 11:14:35 +0100

    use WS4.x only

--- tools/build-zafir.fsx
+++ tools/build-zafir.fsx
@@ -22,7 +22,7 @@ let version = File.ReadAllText(__SOURCE_DIRECTORY__ + "/version.txt")
 let v = Version.Parse version
 
 let bt =
-    BuildTool().PackageId("WebSharper.BabylonJS", version).VersionFrom("WebSharper")
+    BuildTool().PackageId("WebSharper.BabylonJS", version).VersionFrom("WebSharper", "", "[4.0,5.0)")
     |> PackageVersion.Full.Custom v
 
 let asmVersion =
